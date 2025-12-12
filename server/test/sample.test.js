b8d32eb260ef4289603d3e8ad0b0f285665f3e66
santhoshlangesan-sys <santhosh.langesan@email.com>
2025-12-12 13:46:19 +0530

    Test Cases

---
+++ server/test/sample.test.js
@@ -0,0 +1,8 @@
+
+import assert from 'assert';
+
+describe('Sample Test', function() {
+  it('should return true', function() {
+    assert.strictEqual(true, true);
+  });
+});
---
+++ server/test/user.test.js
@@ -0,0 +1,12 @@
+
+import assert from 'assert';
+
+describe('User API', function() {
+  it('should return 2 + 2 = 4', function() {
+    assert.strictEqual(2 + 2, 4);
+  });
+
+  it('should check that true is not false', function() {
+    assert.notStrictEqual(true, false);
+  });
+});
